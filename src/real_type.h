e8edc88505b1ad38897f43ca84e27fd01cd69cbd
Pierre Kestener <pierre.kestener@cea.fr>
2022-05-18 13:26:13 +0200

    cleanup

--- src/real_type.h
+++ src/real_type.h
@@ -64,35 +64,10 @@ constexpr auto ONE_FOURTH_F  = ONE_FOURTH_v<real_t>;
 
 // math function
 #if defined(USE_DOUBLE) ||  defined(USE_MIXED_PRECISION)
-// #define FMAX(x,y) fmax(x,y)
-// #define FMIN(x,y) fmin(x,y)
-// #define SQRT(x) sqrt(x)
-// #define FABS(x) fabs(x)
 #define COPYSIGN(x,y) copysign(x,y)
-// #define ISNAN(x) isnan(x)
-// #define FMOD(x,y) fmod(x,y)
-// #define ZERO_F (0.0)
-// #define HALF_F (0.5)
-// #define ONE_FOURTH_F (0.25)
-// #define ONE_F  (1.0)
-// #define TWO_F  (2.0)
 #else
-// #define FMAX(x,y) fmaxf(x,y)
-// #define FMIN(x,y) fminf(x,y)
-// #define SQRT(x) sqrtf(x)
-// #define FABS(x) fabsf(x)
 #define COPYSIGN(x,y) copysignf(x,y)
-// #define ISNAN(x) isnanf(x)
-// #define FMOD(x,y) fmodf(x,y)
-// #define ZERO_F (0.0f)
-// #define HALF_F (0.5f)
-// #define ONE_FOURTH_F (0.25f)
-// #define ONE_F  (1.0f)
-// #define TWO_F  (2.0f)
 #endif // USE_DOUBLE
-
-// other usefull macros
-#define SQR(x) ((x)*(x))
 
 } // namespace euler2d
 
